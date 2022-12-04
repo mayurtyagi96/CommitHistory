7e328da06ef4cbd4d6365856d891cf334a10ca72
Mayur <mayur.kant.96@gmail.com>
2022-12-04 18:03:34 +0530

    cell connected to VC

--- CommitHistory/ViewController.swift
+++ CommitHistory/ViewController.swift
@@ -9,10 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    
     var network = NetworkManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
 
         network.apiCall { model, isError in
             if isError{
@@ -27,4 +31,21 @@ class ViewController: UIViewController {
     
     
 }
-
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CommitTableViewCell", for: indexPath) as! CommitTableViewCell
+        cell.populateData(title: "commit Successful", imageUrl: "", name: "MAYUR", date: "23-03-2000")
+        return cell
+    }
+    
+    
+    
+    
+    
+    
+    
+}
